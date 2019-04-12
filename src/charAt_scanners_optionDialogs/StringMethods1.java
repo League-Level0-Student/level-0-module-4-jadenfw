dc61ac16fceaaf1dd47a71d401e803de16086868
League <league@WTS9.attlocal.net>
2019-04-12 15:04:58 -0700

    petp1 pi string methods
    :smile:

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,31 +1,38 @@
 package charAt_scanners_optionDialogs;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
+import javax.swing.JOptionPane;
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String string = ("how are you?");
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+		// HINT: .charAt
+		System.out.println(string.charAt(2));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-
+		// HINT: .length()
+		System.out.println(string.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
+		// HINT: use .length() to determine how many loops
+		for (int i = 0; i < string.length(); i++) {
+			System.out.println(string.charAt(i));
+		}
 
-		
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		// what position/index in the String the char is located.
+		// Print the char's position to the console.
+		// EXAMPLE: if your string is "abc" and you are searching
+		// for char 'b', then print "b is at index 1"
+
+		for (int i = 0; i < string.length(); i++) {
+			string.charAt(i);
+			if (string.charAt(i) == 'r') {
+				System.out.println("r is at postition " + i);
+			}
+		}
+
 	}
 }
-
-
